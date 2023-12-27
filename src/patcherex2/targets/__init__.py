4d7580e94ee6c4c4f36e52eee285cfb7bb2fc791
Nick Andry <nicholas.andry33@gmail.com>
2023-12-27 12:24:35 -0500

    implemented x86_64 with tests

--- src/patcherex2/targets/__init__.py
+++ src/patcherex2/targets/__init__.py
@@ -2,6 +2,7 @@ from .elf_aarch64_linux import ElfAArch64Linux
 from .elf_arm_linux import ElfArmLinux
 from .elf_arm_mimxrt1052 import ElfArmMimxrt1052
 from .elf_leon3_bare import ElfLeon3Bare
+from .elf_x86_64_linux import ElfX8664Linux
 from .ihex_ppc_bare import IHexPPCBare
 from .target import Target
 
@@ -10,6 +11,7 @@ __all__ = [
     "ElfArmLinux",
     "ElfArmMimxrt1052",
     "ElfLeon3Bare",
+    "ElfX8664Linux",
     "IHexPPCBare",
     "Target",
 ]
