e88fa6d3ab44954295b27c04daafdac3242c57aa
Nick Andry <nicholas.andry33@gmail.com>
2024-01-19 15:47:15 -0500

    distinguished mips and mips64 in detection

--- src/patcherex2/targets/elf_mips64_linux.py
+++ src/patcherex2/targets/elf_mips64_linux.py
@@ -20,7 +20,7 @@ class ElfMips64Linux(Target):
         with open(binary_path, "rb") as f:
             magic = f.read(0x14)
             print(magic)
-            if magic.startswith(b"\x7fELF") and magic.startswith(
+            if magic.startswith(b"\x7fELF\x02") and magic.startswith(
                 b"\x00\x08", 0x12
             ):  # EM_MIPS
                 return True
