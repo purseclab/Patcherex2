5224f7177bd139677bc570168a3dd33a8e12ba47
Nick Andry <nicholas.andry33@gmail.com>
2024-01-28 23:12:10 -0500

    started ppc64

--- src/patcherex2/targets/__init__.py
+++ src/patcherex2/targets/__init__.py
@@ -7,6 +7,7 @@ from .elf_leon3_bare import ElfLeon3Bare
 from .elf_mips64_linux import ElfMips64Linux
 from .elf_mips_linux import ElfMipsLinux
 from .elf_ppc_linux import ElfPpcLinux
+from .elf_ppc64_linux import ElfPpc64Linux
 from .elf_x86_64_linux import ElfX8664Linux
 from .elf_x86_64_linux_recomp import ElfX8664LinuxRecomp
 from .ihex_ppc_bare import IHexPPCBare
@@ -22,6 +23,7 @@ __all__ = [
     "ElfMips64Linux",
     "ElfMipsLinux",
     "ElfPpcLinux",
+    "ElfPpc64Linux",
     "ElfX8664Linux",
     "ElfX8664LinuxRecomp",
     "IHexPPCBare",
