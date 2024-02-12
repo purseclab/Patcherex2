91bff3d953601c00b7741ec8f718461cdbbe744d
Nick Andry <nicholas.andry33@gmail.com>
2024-02-12 17:30:06 -0500

    implemented and added tests for mips64el

--- src/patcherex2/targets/__init__.py
+++ src/patcherex2/targets/__init__.py
@@ -5,6 +5,7 @@ from .elf_arm_mimxrt1052 import ElfArmMimxrt1052
 from .elf_i386_linux import ElfI386Linux
 from .elf_leon3_bare import ElfLeon3Bare
 from .elf_mips64_linux import ElfMips64Linux
+from .elf_mips64el_linux import ElfMips64elLinux
 from .elf_mips_linux import ElfMipsLinux
 from .elf_ppc64_linux import ElfPpc64Linux
 from .elf_ppc_linux import ElfPpcLinux
@@ -21,6 +22,7 @@ __all__ = [
     "ElfI386Linux",
     "ElfLeon3Bare",
     "ElfMips64Linux",
+    "ElfMips64elLinux",
     "ElfMipsLinux",
     "ElfPpc64Linux",
     "ElfPpcLinux",
