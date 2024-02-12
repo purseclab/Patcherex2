3727f7e2286d033babdbbf2de5fd2c0700a55a65
Han Dai <github@daihan.me>
2024-02-12 02:38:11 -0500

    add some notes

--- src/patcherex2/components/compilers/compiler.py
+++ src/patcherex2/components/compilers/compiler.py
@@ -78,7 +78,17 @@ class Compiler:
             ld = cle.Loader(
                 os.path.join(td, "obj_linked.o"), main_opts={"base_addr": 0x0}
             )
+            # TODO: cle will stop at the beginning of the first unallocated region
+            # found, or when `ld.memory.max_addr` bytes have been read.
+            # So if there is no gap between .text and the next section, cle will
+            # include the next section in the compiled code as well.
+
+            # text_section = next(
+            #     (s for s in ld.main_object.sections if s.name == ".text"), None
+            # )
             compiled = ld.memory.load(
-                ld.all_objects[0].entry + base, ld.memory.max_addr
+                ld.all_objects[0].entry + base,
+                ld.memory.max_addr,
+                # (text_section.vaddr + text_section.memsize),
             )
         return compiled
