c66db41ed4a023c6caa0c0e52a933f0a4a49c98d
Caleb Helbling <caleb.helbling@yahoo.com>
2024-04-23 13:22:57 -0400

    Added test case for IIP C subregister feature

--- tests/test_x86_64.py
+++ tests/test_x86_64.py
@@ -330,6 +330,32 @@ class Tests(unittest.TestCase):
             expected_returnCode=0
         )
 
+    def test_insert_instruction_patch_c_subreg(self):
+        # Original computation was (n + 2) * 2 where n = 4
+        # New computation inserts a factorial step at the beginning
+        # The new computation is (n! + 2) * 2 where n=4
+        instrs = """
+        uint32_t n = eax;
+        for (uint32_t i = 1; i < n; i++) {
+            eax *= i;
+        }
+        """
+
+        config = InsertInstructionPatch.CConfig(
+            scratch_regs=[
+                'r8', 'r9', 'r10', 'r11', 'r13', 'r14', 'r15'
+                'xmm0', 'xmm1', 'xmm2', 'xmm3', 'xmm4', 'xmm5', 'xmm6', 'xmm7', 'xmm9', 'xmm10', 'xmm11', 'xmm12', 'xmm13', 'xmm14', 'xmm15'
+            ],
+            sub_regs=['eax']
+        )
+
+        self.run_one(
+            "iip_c",
+            [InsertInstructionPatch(0x1157, instrs, language="C", c_config=config)],
+            expected_output=b"52",
+            expected_returnCode=0
+        )
+
     def run_one(
         self,
         filename,
