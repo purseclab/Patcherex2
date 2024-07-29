874b56d7aae12ea798e8fa25ccc297333fe55b2e
Caleb Helbling <chelbling@draper.com>
2024-07-29 16:21:18 -0400

    Synchronize aarch64 preserve_none register list with what's in the LLVM repo/codebase

--- src/patcherex2/components/archinfo/aarch64.py
+++ src/patcherex2/components/archinfo/aarch64.py
@@ -50,10 +50,7 @@ class Aarch64Info:
 
     cc = {
         "Linux": ["x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7"],
-        # TODO: update LinuxPreserveNone once aarch64 support lands
-        # in LLVM for preserve_none, currently defaults to Linux
         "LinuxPreserveNone": [
-            "x19",
             "x20",
             "x21",
             "x22",
@@ -71,13 +68,12 @@ class Aarch64Info:
             "x5",
             "x6",
             "x7",
-            "x9",
             "x10",
             "x11",
             "x12",
             "x13",
             "x14",
-            "x15"
+            "x9",
         ],
     }
     callee_saved = {
