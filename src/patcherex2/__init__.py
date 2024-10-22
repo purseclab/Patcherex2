f0739a7c600aa74442287c81a9c3716111f8bfaf
Han Dai <github@daihan.me>
2024-10-22 19:22:30 -0400

    use setuptools_scm

--- src/patcherex2/__init__.py
+++ src/patcherex2/__init__.py
@@ -1,5 +1,8 @@
 # ruff: noqa
 from .patcherex import Patcherex
 from .patches import *
+from importlib import metadata
+
+__version__ = metadata.version("patcherex2")
 
 __all__ = ["Patcherex"] + patches.__all__
